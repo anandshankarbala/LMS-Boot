2954453c81a636785165f6e99be5f2e0be02e765
anandshankarbala <anandshankarbala@gmail.com>
2019-02-06 03:16:29 +0530

    book by title

--- LmsBoot/src/main/java/com/anand/LmsBoot/repository/BookRepository.java
+++ LmsBoot/src/main/java/com/anand/LmsBoot/repository/BookRepository.java
@@ -1,7 +1,11 @@
 package com.anand.LmsBoot.repository;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.anand.LmsBoot.model.Book;
 
 public interface BookRepository<P> extends CrudRepository<Book, Long> {
+	List<Book> findByBookTitle(String bookTitle);
+	
 }
--- LmsBoot/src/main/java/com/anand/LmsBoot/rest/BookController.java
+++ LmsBoot/src/main/java/com/anand/LmsBoot/rest/BookController.java
@@ -7,6 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.anand.LmsBoot.model.Book;
@@ -22,6 +25,13 @@ public class BookController {
 	
 	@GetMapping("/books")
 	public List<Book> getAllBooks(){
+		log.debug("get all books");
 		return (List<Book>) bookRepository.findAll();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET,value="/getBooksByTitle/{bookTitle}")
+	public List<Book> getBooksByName(@PathVariable("bookTitle") String bookTitle){
+		log.debug("get all books bookTitle");
+		return (List<Book>) bookRepository.findByBookTitle(bookTitle);
 	}
 }
