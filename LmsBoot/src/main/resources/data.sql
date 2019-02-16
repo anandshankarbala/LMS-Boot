5a15cf279f2cc41070490ab86d35cb5ff76dbb82
anandshankarbala <anandshankarbala@gmail.com>
2019-02-17 00:50:32 +0530

    table name change for members to users

--- LmsBoot/src/main/resources/data.sql
+++ LmsBoot/src/main/resources/data.sql
@@ -3,4 +3,4 @@ INSERT INTO `book` (`id`, `ISBN_NO`, `Book_Title`, `Author_Name`, `Language`,`Pu
 (2, '1234567129391', 'Mayavalai', 'Pa Ragavan','Tamil', '2018-02-24', NULL, '500.00', 432, 'Mathi Nilayam'),
 (3, '81-7371-146-1', 'Wings of Fire', '	A P J Abdul Kalam','English', '2018-02-24', NULL, '450.00', 133, 'Scholastic Corporation');
 
-INSERT INTO `member`(`username`, `email`, `password`,`first_name`,`address`) VALUES ('admin','andshr.2020@gmail.com','root','Anand','Pondy');
+INSERT INTO `users`(`username`, `email`, `password`,`first_name`,`address`) VALUES ('admin','andshr.2020@gmail.com','root','Anand','Pondy');
