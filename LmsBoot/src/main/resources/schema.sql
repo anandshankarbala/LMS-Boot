922d343a0df00ed4fee38e4fe819b5281fccd8ae
anandshankarbala <anandshankarbala@gmail.com>
2019-02-16 21:43:55 +0530

    Changes in schema and data

--- LmsBoot/src/main/resources/schema.sql
+++ LmsBoot/src/main/resources/schema.sql
@@ -2,12 +2,13 @@
 -- GET USER CONTACT DETAILS OUT OF USER TABLE
 -- refer http://www.mkyong.com/spring-security/spring-security-form-login-using-database/ for db design
 
-
+--  Create LMS Database 
 CREATE DATABASE IF NOT EXISTS lms;
 use lms;
 
+--  Create Books Table
 CREATE TABLE IF NOT EXISTS `lms`.`book` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(11) NOT NULL,
   `ISBN_NO` varchar(100) DEFAULT NULL,
   `Book_Title` varchar(200) DEFAULT NULL,
   `Author_Name` varchar(100) DEFAULT NULL,
@@ -16,13 +17,15 @@ CREATE TABLE IF NOT EXISTS `lms`.`book` (
   `Edition` varchar(40) DEFAULT NULL,
   `Price` decimal(10,2) DEFAULT '0.00',
   `Pages` int(11) DEFAULT NULL,
-  `Publisher` varchar(140) DEFAULT NULL
+  `Publisher` varchar(140) DEFAULT NULL,
+  PRIMARY KEY ( `id` ) 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 -- ENABLED TINYINT NOT NULL DEFAULT 1  TO DISABLE USER
 -- GET USER CONTACT DETAILS OUT OF USER TABLE
 
+--  Create Users Table
 CREATE TABLE IF NOT EXISTS `lms`.`users` (
 `id` int( 11 ) NOT NULL AUTO_INCREMENT ,
 `username` varchar( 30 ) NOT NULL ,
@@ -35,18 +38,19 @@ UNIQUE KEY `username` ( `username` )
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 
-
+--  Create User Role Table
 CREATE TABLE IF NOT EXISTS `lms`.`user_roles` (
-  user_role_id int(11) NOT NULL AUTO_INCREMENT,
-  user_id int(11) NOT NULL,
-  role varchar(45) NOT NULL,
-  PRIMARY KEY (user_role_id),
-  UNIQUE KEY uni_username_role (role,username),
-  KEY fk_username_idx (username),
-  CONSTRAINT fk_username FOREIGN KEY (user_id) REFERENCES users(id)
+  `user_role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar( 30 ) NOT NULL ,
+  `role` varchar(45) NOT NULL,
+  PRIMARY KEY (`user_role_id`),
+  UNIQUE KEY uni_username_role (`role`,`username`),
+  KEY fk_username_idx (`username`),
+  CONSTRAINT fk_username FOREIGN KEY (`username`) REFERENCES users(`username`)
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+--  Create Book Copy Table which tells abouts which book issued by whom
 CREATE TABLE IF NOT EXISTS `lms`.`book_copy` (
 `id` int( 11 ) NOT NULL AUTO_INCREMENT ,
 `book_id` int( 11 ) NOT NULL,
@@ -73,4 +77,3 @@ FOREIGN KEY (user_id) REFERENCES users(id);
 ALTER TABLE checkout
 ADD CONSTRAINT FK_BookCopyCheckout
 FOREIGN KEY (book_copy_id) REFERENCES book_copy(id);
-
